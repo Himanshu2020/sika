172d890dc00139ddec131af969dc7f1653f4016e
Himanshu <techlink.project@gmail.com>
2018-08-05 17:05:33 +0530

    hima

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "s";
+    const std::string addressPrefix = "sika";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "SIKA";
